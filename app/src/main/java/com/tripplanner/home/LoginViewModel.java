5444b65e38206e6f1a4411c358576a8f7d7e0b59
Your Name <sarazedan287@gmail.com>
2020-02-19 14:24:18 -0800

    insert user into database

--- app/src/main/java/com/tripplanner/home/LoginViewModel.java
+++ app/src/main/java/com/tripplanner/home/LoginViewModel.java
@@ -4,14 +4,32 @@ package com.tripplanner.home;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.tripplanner.data_layer.Repository;
+import com.tripplanner.data_layer.local_data.Entity.User;
 
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
-  //  private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-   // public MutableLiveData<FirebaseUser> mutableLiveData=new MutableLiveData<>();
-    public FirebaseUser getCurrentUser() {
-
-        return  FirebaseAuth.getInstance().getCurrentUser();
-    }
+  Repository repository=new Repository();
+//save user to firebase database
+   public void savetUser(FirebaseUser firebaseUser)
+   {
+       User user= mapFirebaseUserIntoUser( firebaseUser);
+       insertUser(user);
+   }
+   //map FirebaseUser Into User entity
+   public User mapFirebaseUserIntoUser(FirebaseUser firebaseUser)
+   {
+       User currentUser=new User(firebaseUser.getUid(),firebaseUser.getDisplayName(),firebaseUser.getPhotoUrl().toString());
+       return currentUser;
+   }
+   public  void  insertUser(User user)
+   {
+       repository.insertUser(user);
+   }
+   public void setCurrentUser(FirebaseUser firebaseUser)
+   {
+       User currentUser=new User(firebaseUser.getUid(),firebaseUser.getDisplayName(),firebaseUser.getPhotoUrl().toString());
+       repository.setCurrentUser(currentUser);
+   }
 }
